0db8fec2b30c9ec67fe9552b9a75f373abe6af2d
François-Xavier Thoorens <fx.thoorens@gmail.com>
2024-07-16 09:55:51 +0200

    Deploy website - based on cdfb4d098912f72871a351954e9053e369f7a31d

---
+++ zh/assets/js/739e32bb.3712660c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksmartlink_doc=self.webpackChunksmartlink_doc||[]).push([[9828],{15652:a=>{a.exports=JSON.parse('{"tags":[{"label":"SmartLink","permalink":"/zh/blog/tags/smartlink","description":"SmartLink\u662f\u6211\u4eec\u7684\u65d7\u8230SaaS\uff0c\u7528\u4e8e\u4fdd\u62a4\u60a8\u7684\u516c\u53f8\u3002","count":8}]}')}}]);
